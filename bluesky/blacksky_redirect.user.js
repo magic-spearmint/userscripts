4c517ae1b0c2ca029184c2bf542d4bf985086e47
Magic Spearmint <3399190+magic-spearmint@users.noreply.github.com>
2025-11-30 02:52:05 -0600

    Update blacksky_redirect.user.js

--- bluesky/blacksky_redirect.user.js
+++ bluesky/blacksky_redirect.user.js
@@ -1,20 +1,31 @@
 // ==UserScript==
 // @name        Bluesky: Blacksky Redirect
 // @namespace   magic_spearmint-bluesky-blacksky_redirect
-// @author      Magic Spearmint
 // @description Automatically redirects Bluesky pages serving comptaible ATProto content to their equivalent Blacksky pages.
+// @author      Magic Spearmint
 // @copyright   2025 Magic Spearmint
-// @version     0.1.0
-// @license     https://creativecommons.org/publicdomain/zero/1.0
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=bsky.app
-// @homepageURL https://github.com/magic-spearmint/userscripts
-// @supportURL  https://github.com/magic-spearmint/userscripts/wiki
-// @downloadURL https://github.com/magic-spearmint/userscripts/main/bluesky/blacksky_redirect.user.js
-// @updateURL   https://github.com/magic-spearmint/userscripts/main/bluesky/blacksky_redirect.meta.js
+// @license     https://creativecommons.org/publicdomain/zero/1.0
+// @version     0.1.1
 // @match       *://bsky.app/*
 // @match       *://fxbsky.app/*
 // @exclude     *://cdn.bsky.app/*
 // @run-at      document-start
+// @compatible  arc       Tampermonkey or Violentmonkey
+// @compatible  brave     Tampermonkey or Violentmonkey
+// @compatible  chrome    Tampermonkey or Violentmonkey
+// @compatible  chromium  Tampermonkey or Violentmonkey
+// @compatible  edge      Tampermonkey or Violentmonkey
+// @compatible  firefox   Greasemonkey, Tampermonkey or Violentmonkey
+// @compatible  librewolf Greasemonkey, Tampermonkey or Violentmonkey
+// @compatible  opera     Tampermonkey or Violentmonkey
+// @compatible  vivaldi   Tampermonkey or Violentmonkey
+// @compatible  waterfox  Greasemonkey, Tampermonkey or Violentmonkey
+// @compatible  yandex    Tampermonkey or Violentmonkey
+// @homepageURL https://github.com/magic-spearmint/userscripts
+// @supportURL  https://github.com/magic-spearmint/userscripts/wiki
+// @updateURL   https://github.com/magic-spearmint/userscripts/main/bluesky/blacksky_redirect.meta.js
+// @downloadURL https://github.com/magic-spearmint/userscripts/main/bluesky/blacksky_redirect.user.js
 // ==/UserScript==
 
 /* jshint esversion: 8 */
